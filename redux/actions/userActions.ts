979203de4c3335d7cbf4cd91116caf56bc3ad4d0
Hasan Şişik <sisik.hsn@gmail.com>
2025-05-14 21:46:28 +0300

    redux

---
+++ redux/actions/userActions.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { server } from "../../config";
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export const login = createAsyncThunk(
+  "user/login",
+  async (payload: LoginPayload, thunkAPI) => {
+    try {
+      const { data } = await axios.post(`${server}/auth/login`, payload);
+      const token = data.user.token;
+      localStorage.setItem("accessToken", token);
+      document.cookie = `token=${token}; path=/`;
+      return data.user;
+    } catch (error: any) {
+      const message = error.response?.data?.message || 'Giriş yapılamadı';
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
---
+++ redux/reducers/userReducer.ts
@@ -0,0 +1,41 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  login,
+} from "../actions/userActions";
+
+interface userState {
+  user: any;
+  loading: boolean;
+  error: string | null;
+  isAuthenticated?: boolean;
+  message?: string;
+  users?: any[];
+  notes?: any[];
+}
+
+const initialState: userState = {
+  user: {},
+  loading: false,
+  error: null,
+  users: [],
+  notes: [],
+};
+
+export const courseReducer = createReducer(initialState, (builder) => {
+  builder
+    // Login
+    .addCase(login.pending, (state) => {
+      state.loading = true;
+    })
+    .addCase(login.fulfilled, (state, action) => {
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = action.payload;
+    })
+    .addCase(login.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload as string;
+    })
+});
+
+export default courseReducer;
